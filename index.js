603147245174ab7c6e667670d56a803a9af4d172
root <saung.myat@gmail.com>
2022-03-01 21:07:33 -0500

    commit tree eefca7c6ac01fdd4b8ca87bd717c1961acf0d868

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someBlocks) {
+    return Math.abs(42 - someBlocks);
+}
+
+function distanceFromHqInFeet(someFeet) {
+    return Math.abs(42 - someFeet) * 264;
+}
+
+function distanceTravelledInFeet(startFeet, endFeet) {
+    return Math.abs(startFeet - endFeet) * 264;
+}
+
+function calculatesFarePrice(startFare, endFare) {
+    if (Math.abs(startFare - endFare) * 264 <= 400)
+        {
+             return 0;
+        }
+    else if (Math.abs(startFare - endFare) * 264 > 400 && Math.abs(startFare - endFare) * 264 <= 2000)
+        {
+            return ((Math.abs(startFare - endFare) * 264) -400)* 0.02;
+        }
+    else if (Math.abs(startFare - endFare) * 264 > 2000 && Math.abs(startFare - endFare) * 264 <= 2500)
+        {
+            return 25;
+        }
+    else if (Math.abs(startFare - endFare) * 264 > 2500)
+        {
+            return "cannot travel that far";
+        }
+}
